46ae99f00621cfac77c0413f8419da3965c692c4
Richard Mitchell <richard.j.mitchell@gmail.com>
2011-01-25 13:10:35 +0000

    Removed temp file, .pycs and updated version number in setup.py to match intended version number.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='isotoma.zope.testpythonscript',
-    version="0.0.1",
+    version="1.0.0",
     url="http://www.isotoma.com/",
     author="Richard Mitchell",
     author_email="richard.mitchell@isotoma.com",
